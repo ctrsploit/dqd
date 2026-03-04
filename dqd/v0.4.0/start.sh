8e1049dd861239cff94725e67955e8c2b11e2051
LEI WANG <ssst0n3@gmail.com>
2026-03-04 16:21:18 +0800

    dqd base image

---
+++ dqd/v0.4.0/start.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+set -eu  # Exit on error or use of undefined variable
+
+build_default_net_args() {
+    local opts="-nic user"
+
+    # setup ip
+    [ -n "${QEMU_NET:-}" ] && opts="${opts},net=${QEMU_NET}"
+    [ -n "${QEMU_DHCPSTART:-}" ] && opts="${opts},dhcpstart=${QEMU_DHCPSTART}"
+
+    # Always enable SSH port forwarding
+    opts="${opts},hostfwd=tcp::22-:22"
+
+    # Additional port forwarding, if defined
+    [ -n "${QEMU_HOSTFWD:-}" ] && opts="${opts},${QEMU_HOSTFWD}"
+
+    # Set MAC address for the first NIC, if defined
+    [ -n "${QEMU_MAC_ADDR_0:-}" ] && opts="${opts},mac=${QEMU_MAC_ADDR_0}"
+
+    echo "$opts,model=virtio"
+}
+
+# Build dual-network arguments (socket-based)
+build_dual_net_args() {
+    local opts="-nic socket"
+
+    [ -n "${QEMU_SOCKET_LISTEN:-}" ] && opts="${opts},listen=${QEMU_SOCKET_LISTEN}"
+    [ -n "${QEMU_SOCKET_CONNECT:-}" ] && opts="${opts},connect=${QEMU_SOCKET_CONNECT}"
+    # Set MAC address for the second NIC, if defined
+    [ -n "${QEMU_MAC_ADDR_1:-}" ] && opts="${opts},mac=${QEMU_MAC_ADDR_1}"
+    echo "$opts,model=virtio"
+}
+
+build_cloudinit_args() {
+#     cat > /user-data <<EOF
+# #cloud-config
+# runcmd:
+#   - ip addr add ${QEMU_SOCKET_IP}/24 dev eth1
+#   - ip link set eth1 up
+# EOF
+
+# setup optional=true to prevent network-online.target stuck 
+    cat > /network-config <<EOF
+network:
+  version: 2
+  ethernets:
+    eth1:
+      dhcp4: false
+      addresses:
+        - ${QEMU_SOCKET_IP}/24
+      optional: true
+EOF
+    cloud-localds --network-config=/network-config /seed.img /dev/null
+    echo "-drive file=/seed.img,if=virtio,format=raw"
+}
+
+CMD="qemu-system-x86_64 \
+    -hda vm.qcow2 \
+    -smp 2 \
+    -m 2048M \
+    -nographic"
+
+CMD="$CMD $(build_default_net_args)"
+
+if [ -n "${QEMU_DUAL_NET:-}" ]; then
+    CMD="$CMD $(build_dual_net_args)"
+fi
+
+if [ -n "${QEMU_SOCKET_IP:-}" ]; then
+    CMD="$CMD $(build_cloudinit_args)"
+fi
+
+# setup kvm
+if [ -e /dev/kvm ]; then
+    CMD="$CMD -enable-kvm"
+fi
+
+exec $CMD "$@"
